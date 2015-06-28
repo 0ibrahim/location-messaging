49ac4b27710dd290b0882c9ee624c367d43cad12
Forest Tong <ftong@mit.edu>
2015-06-27 19:10:02 -0700

    before pulling

--- db.js
+++ db.js
@@ -7,6 +7,11 @@ function createGroup(groupName) {
 			latitude = position.coords.latitude;
 			longitude = position.coords.longitude;
 			firebaseRef.push({name: groupName, location: [latitude, longitude]});
+			geoFire.set(groupName, [latitude, longitude]).then(function() {
+			  console.log("Provided key has been added to GeoFire");
+			}, function(error) {
+			  console.log("Error: " + error);
+			});
 		})
 	} else {
 		alert("Geolocation is not supported!");
