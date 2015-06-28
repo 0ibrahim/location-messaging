43ef27647d2878075a3587f98ad4720fbd33bf25
Forest Tong <ftong@mit.edu>
2015-06-27 19:45:16 -0700

    changed directory structure

---
+++ js/db.js
@@ -0,0 +1,57 @@
+var firebaseRef = new Firebase("https://luminous-torch-1983.firebaseio.com/");
+var geoFire = new GeoFire(firebaseRef);
+
+var RADIUS = 10.5;
+
+function createGroup(groupName) {
+	if(navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+			var latitude = position.coords.latitude;
+			var longitude = position.coords.longitude;
+			firebaseRef.push({name: groupName, location: [latitude, longitude]});
+			geoFire.set(groupName, [latitude, longitude]).then(function() {
+			  console.log("Provided key has been added to GeoFire");
+			}, function(error) {
+			  console.log("Error: " + error);
+			});
+		})
+	} else {
+		alert("Geolocation is not supported!");
+	}
+}
+
+function getGroups() {
+	if(navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(getGroupFromPosition);
+	} else {
+		alert("Geolocation is not supported!");
+	}
+}
+
+// Should not be called multiple times
+function getGroupFromPosition(position) {
+	var geoQuery = geoFire.query({
+		center: [position.coords.latitude, position.coords.longitude],
+		radius: RADIUS,
+	});
+	var onReadyRegistration = geoQuery.on("ready", function() {
+	  console.log("GeoQuery has loaded and fired all other events for initial data");
+	});
+	var onKeyEnteredRegistration = geoQuery.on("key_entered", function(key, location, distance) {
+	  console.log(key + " entered query at " + location + " (" + distance + " km from center)");
+	});
+}
+
+document.getElementById('new-group').onclick = function() {createGroup('testing')};
+
+function addMessageToGroup(groupID, data, format) {
+	if(groupID && data) {
+		var groupRef = firebaseRef.child(groupID);
+		var messagesRef = groupRef.child(messages);
+		messagesRef.push({userid: auth.id, format: format, data: data});	
+	}
+	else {
+		alert("Error");
+		return
+	}
+}
